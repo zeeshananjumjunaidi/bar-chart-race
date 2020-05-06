29d020ee96524e13b9cece98a3c9f753e0911c1e
Zeeshan Anjum <zeeshan.anjum@eureka.me>
2020-05-06 12:23:00 +0300

    Added sorting animation

--- race-bar.js
+++ race-bar.js
@@ -21,7 +21,7 @@ function sleep(miliseconds) {
     }
 }
 
-sortable_bars=[];
+sortable_bars = [];
 async function init() {
     let graphs = document.getElementsByClassName('race-bar');
     let graphs_items = [];
@@ -33,7 +33,9 @@ async function init() {
             l.className = "bar";
             let width = 0;// ((Math.random() * 100 * SCALE) + 100);
             l.style.width = width;
+            l.style.top = i*20;
             l.setAttribute("size", width);
+            l.setAttribute("name", getName(4));
 
             l.style.background = getColor();
             let text = document.createTextNode((i + 1) + "# Node");
@@ -49,29 +51,23 @@ async function init() {
 
     for (let item of graphs_items) {
         let safe_counter = 10000;
-        for (let graph of item.childNodes) {            
-            sortable_bars=graph.children;
+        for (let graph of item.childNodes) {
+            sortable_bars = graph.children;
             for (let i = 0; i < graph.children.length; i++) {
                 let ele = graph.children[i];
-                let maxVal = parseInt(ele.getAttribute("max-value"));
-
                 animateLifespan(ele);
-                // for (let j = 0; j < graph.children.length; j++) {
-                //     for (let k = 0; k < graph.children.length; k++) {
-                //     let ele1 = graph.children[j];
-                //     let ele2 = graph.children[k];
-                //     let width1 = parseInt(ele1.getAttribute("max-value"));
-                //     let width2 = parseInt(ele2.getAttribute("max-value"));
-                //         if(width1<width2){
-                //             swap(ele1,ele2,graph);
-                //         }
-                //     }
-                // }
             }
         }
     }
 }
-
+function getName(length) {
+    let name = '';
+    for (let i = 0; i < length; i++) {
+        let char = String.fromCharCode(65 + parseInt(Math.random() * 24));
+        name += char;
+    }
+    return name;
+}
 function getColor() {
     let colorValue1 = Math.floor(Math.random() * 16777215);
     let colorValue2 = Math.floor(Math.random() * 16777215);
@@ -82,7 +78,55 @@ function getColor() {
     //  console.debug(gradient);
     return gradient;
 }
-// Credit for swapping function: https://codesandbox.io/s/zen-minsky-bwyr9?from-embed=&file=/src/index.js:688-1248
+
+
+async function bubbleSort(delay = 100) {
+    if (delay && typeof delay !== "number") {
+      alert("sort: First argument must be a typeof Number");
+      return;
+    }
+    let blocks = document.querySelectorAll(".bar");
+    for (let i = 0; i < blocks.length - 1; i += 1) {
+      for (let j = 0; j < blocks.length - i - 1; j += 1) {  
+        await new Promise(resolve =>
+          setTimeout(() => {
+            resolve();
+          }, delay)
+        );
+  
+        const value1 = Number(blocks[j].getAttribute('size'));//.childNodes[0].innerHTML);
+        const value2 = Number(blocks[j + 1].getAttribute('size'));//.childNodes[0].innerHTML);
+  
+        if (value1 > value2) {
+          await swap(blocks[j], blocks[j + 1]);
+          blocks = document.querySelectorAll(".bar");
+        }
+  
+        blocks[j].style.backgroundColor = "#58B7FF";
+        blocks[j + 1].style.backgroundColor = "#58B7FF";
+      }
+  
+      blocks[blocks.length - i - 1].style.backgroundColor = "#13CE66";
+    }
+  }
+
+
+
+
+async function temp(ele1,ele2){
+   let f1 = ele1.style.top;
+   let f2 = ele2.style.top;
+   let width1 = ele1.getAttribute('size');
+   let width2 = ele2.getAttribute('size');
+
+   let animationLength = 1000;   
+   ele1.style.transition = `top ${animationLength}ms`;
+   ele2.style.transition = `top ${animationLength}ms`;  
+   ele1.style.top =f2;//
+   ele2.style.top =f1;// 
+ 
+}
+
 function swap(el1, el2, container) {
     return new Promise(resolve => {
         const style1 = window.getComputedStyle(el1);
@@ -99,32 +143,40 @@ function swap(el1, el2, container) {
             setTimeout(() => {
                 container.insertBefore(el2, el1);
                 resolve();
-            }, 250);
+            }, 5);
         });
     });
 }
 
 
-let framesPerSecond = 90;
+let framesPerSecond = 120;
 
-async function sort(container){
-    console.log("swappoing");
-      for (let j = 0; j < sortable_bars.length; j++) {
-                for (let k = 0; k < sortable_bars.length; k++) {
-                    let ele1 = sortable_bars[j];
-                    let ele2 = sortable_bars[k];
+async function sort() {
+    let elements = document.getElementsByClassName("race-bar");
+    for (let elment of elements) {
+        console.log(elements);
+        console.log("sorting");
+        for (let box of elment.children) {
+            for (let ele1 of box.children) {
+                let name1 = ele1.getAttribute("name");
+                for (let ele2 of box.children) {
+                    let name2 = ele2.getAttribute("name");
                     let width1 = parseInt(ele1.getAttribute("size"));
                     let width2 = parseInt(ele2.getAttribute("size"));
-                    if(width1<width2){
-                         await swap(ele1,ele2,container);
+                    if (width1 < width2) {
+                        console.log("Swapping", name1, name2, width1, width2);
+                        await temp(ele1, ele2);
+                    }
                 }
             }
         }
+    }
 }
 async function animateLifespan(ele) {
     return new Promise(async (resolve) => {
         let width = parseInt(ele.getAttribute("size"));
         let maxVal = parseInt(ele.getAttribute("max-value"));
+        let name = ele.getAttribute("name");
         let running = true;
         let f = function animate() {
             if (running) {
@@ -137,27 +189,47 @@ async function animateLifespan(ele) {
                     }
                     ele.setAttribute("size", width);
                     ele.style.width = width;
-                    ele.innerText = `${width}/${maxVal}`;                  
+                    ele.innerHTML = `<strong>${name} ▶</strong>: ${width}/${maxVal}`;
                     if (width == maxVal) {
                         console.log("Stopping ", tId);
                         running = false;
                         clearTimeout(tId);
                         return resolve();
                     }
-
-
-
                 }, 1000 / framesPerSecond);
             }
         }
-        let s =async function sortAnim(){
-            if(running){                
+        let s = async function sortAnim() {
+            if (running) {
                 requestAnimationFrame(sortAnim);
                 await sort(ele.parentNode);
             }
         }
 
         f();
-        s();
+        // s();
     });
+
+
+    
+function swap(el1, el2) {
+    return new Promise(resolve => {
+      const style1 = window.getComputedStyle(el1);
+      const style2 = window.getComputedStyle(el2);
+  
+      const transform1 = style1.getPropertyValue("transform");
+      const transform2 = style2.getPropertyValue("transform");
+  
+      el1.style.transform = transform2;
+      el2.style.transform = transform1;
+  
+      // Wait for the transition to end!
+      window.requestAnimationFrame(function() {
+        setTimeout(() => {
+          container.insertBefore(el2, el1);
+          resolve();
+        }, 250);
+      });
+    });
+  }
 }
